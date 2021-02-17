3c75ae3c7b9763a908134e7347c8621322f96611
Wesley Machado <weslley182@gmail.com>
2021-02-17 16:09:10 -0300

    função de contaCorrente

--- main.go
+++ main.go
@@ -9,6 +9,16 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
 		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
@@ -25,7 +35,12 @@ func main() {
 
 	fmt.Println(contaDoGuilherme)
 	fmt.Println(contaDaBruna)
-	
+
 	fmt.Println(contaDaCris == contaDaCris2)
 	fmt.Println(*contaDaCris == *contaDaCris2)
+
+	fmt.Println(contaDaCris.saldo)
+
+	fmt.Println(contaDaCris.Sacar(400))
+	fmt.Println(contaDaCris.saldo)
 }
