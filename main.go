9eeadcc3baec393a64847864a4fdd39cc9b38b2a
Wesley Machado <weslley182@gmail.com>
2021-02-17 16:37:28 -0300

    separando pacotes

--- main.go
+++ main.go
@@ -1,46 +1,18 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
+import (
+	"Banco/contas"
+	"fmt"
+)
 
 func main() {
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
-		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
 
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	//status := contaDaSilvia.Tranferir(100, &contaDoGustavo)
+	status := contaDoGustavo.Tranferir(100, &contaDaSilvia)
 
-	contaDaCris := new(ContaCorrente)
-	contaDaCris.titular = "Cristiane"
-	contaDaCris.saldo = 5000.95
-
-	contaDaCris2 := new(ContaCorrente)
-	contaDaCris2.titular = "Cristiane"
-	contaDaCris2.saldo = 5000.95
-
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaBruna)
-
-	fmt.Println(contaDaCris == contaDaCris2)
-	fmt.Println(*contaDaCris == *contaDaCris2)
-
-	fmt.Println(contaDaCris.saldo)
-
-	fmt.Println(contaDaCris.Sacar(400))
-	fmt.Println(contaDaCris.saldo)
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
 }
